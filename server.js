9fb0c640d8f9abf5db6d1496bcf3ad54904bac7c
Shivani Bhatia <scbhatia@bu.edu>
2018-03-24 14:07:07 -0400

    fixed sessions updating

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const con = mysql.createConnection({
     host: 'vobot.cbd0j537sw1a.us-east-1.rds.amazonaws.com',
     port: '3306',
     user: 'scbhatia',
-    password: 'ApriL-0696',
+    password: '12345678',
     database: 'vobot'
 });
 
@@ -23,26 +23,236 @@ con.connect(function(err) {
 
 });
 
-// pushing child's name, child's word, level for word, individual simiarlity score, session number to sessions table (adding every time)
+
+// calculates level, overall score, session 
+app.post('/calc_val', function(req,res) {
+    let phone_number = req.body.phone_number;
+    let childs_word = req.body.childs_word;
+    let childs_name = req.body.childs_name;
+    let indiv_score = req.body.indiv_score;
+    var session;
+    var level;
+    var score;
+    var levelbool = false;
+
+    if (!phone_number || !childs_word || !childs_name || !indiv_score) {
+        return res.status(400).send({ error:true, message: 'Please provide task' });
+    }
+
+    con.query('SELECT level, overall_score, overall_sessions FROM progress WHERE phone_number=? AND childs_word=?', [phone_number, childs_word], function(err,results, fields) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            if (results.length > 0) {
+                if (results) {
+                    level = results[0].level;
+                    score = results[0].overall_score;
+                    session = results[0].overall_sessions;
+                    session = session + 1;
+                   // console.log("SCORE: " + score);
+                    score = (score + indiv_score) / 2;
+		   // return res.send({"sessions: ": session, "score: " : score});
+                    switch(level) {
+                        case 2: // 10 - 20
+                        // goes down
+                            if (indiv_score < 10) {
+                                level = 1;
+                            }
+                            // goes up
+                            else if (indiv_score >= 20) {
+                                level = 3;
+                                levelbool = true;
+                            }
+                            // same
+                            else {
+                                level = 2;
+                            }
+                            break;
+        
+                        case 3: // 20-30
+                            // goes down
+                            if (indiv_score < 20) {
+                                level = 2;
+                            }
+                            // goes up
+                            else if (indiv_score >= 30) {
+                                level = 4;
+                                levelbool = true;
+                            }
+                            // same
+                            else {
+                                level = 3;
+                            }
+                            break;
+        
+                        case 4: // 30-40
+                            // down
+                            if (indiv_score < 30) {
+                                level = 3;
+                            }
+                            // up
+                            else if (indiv_score >= 40) {
+                                level = 5;
+                                levelbool = true;
+                            }
+                            // same
+                            else {
+                                level = 4;
+                            }
+                            break;
+        
+                        case 5: // 40-50
+                            // down
+                            if (indiv_score < 40) {
+                                level = 4;
+                            }
+                            // up
+                            else if (indiv_score >= 50) {
+                                level = 6;
+                                levelbool = true;
+                            }
+                            // same
+                            else {
+                                level = 5;
+                            }
+                            break;
+        
+                        case 6: // 50-60
+                            // down
+                            if (indiv_score < 50) {
+                                level = 5;
+                            }
+                            // up
+                            else if (indiv_score >= 60) {
+                                level = 7;
+                                levelbool = true;
+                            }
+                            // same
+                            else {
+                                level = 6;
+                            }
+                            break;
+        
+                        case 7: // 60-70
+                            // down
+                            if (indiv_score < 60) {
+                                level = 6;
+                            }
+                            // up
+                            else if (indiv_score >= 70) {
+                                level = 8;
+                                levelbool = true;
+                            }
+                            // same
+                            else {
+                                level = 7;
+                            }
+                            break;
+        
+                        case 8: //70-80
+                            // goes down
+                            if (indiv_score < 70) {
+                                level = 7;
+                            }
+                            // goes up
+                            else if (indiv_score >= 80) {
+                                level = 9;
+                                levelbool = true;
+                            }
+                            // stays 
+                            else {
+                                level = 8;
+                            }
+                            break;
+        
+                        case 9: //80-90
+                            // goes down
+                            if (indiv_score < 80) {
+                                level = 8;
+                            }
+                            // goes up
+                            else if (indiv_score >= 90) {
+                                level = 10;
+                                levelbool = true;
+                            }
+                            // stays
+                            else {
+                                level = 9;
+                            }
+                            break;
+        
+                        case 10: //90-100
+                            // goes down
+                            if (indiv_score < 90) {
+                                level = 9;
+                            }
+                            // stays
+                            else {
+                                level = 10;
+                            }
+                            break;
+        
+                        default:
+                            level = level;
+                            break;
+                    }
+                    return res.send({error:false, levelbool: levelbool, level: level, session: session, overall_score: score})
+                }
+            }
+            else {
+                level = 1;
+                score = indiv_score;
+                session = 1;
+                // goes up
+                if (indiv_score >= 10) {
+                    level = 2;
+                    levelbool = true;
+                }
+                // same
+                else {
+                    level = 1;
+                }
+                return res.send({error:false, levelbool: levelbool, level: level, session: session, overall_score: score})
+
+            }
+        }
+
+    });
+});
+
+//POSTS TO SESSIONS
 app.post('/sessions', function(req,res) {
     let phone_number = req.body.phone_number;
-    let session = req.body.session;
     let childs_word = req.body.childs_word;
     let childs_name = req.body.childs_name;
     let indiv_score = req.body.indiv_score;
     let level = req.body.level;
- 
-    if (!phone_number || !session || !childs_word || !childs_name || !indiv_score || !level) {
-        return res.status(400).send({ error:true, message: 'Please provide task' });
-    }
+    let session = req.body.session;
 
-    var info = {phone_number: phone_number,  session: session, childs_word: childs_word, childs_name: childs_name, indiv_score: indiv_score, level: level};
-    
-    con.query('INSERT INTO sessions SET ? ', info, function(error, results, fields) {
+    var data = {phone_number: phone_number, session: session, childs_word: childs_word, childs_name: childs_name, indiv_score: indiv_score, level: level};
+    con.query('INSERT IGNORE INTO sessions SET ?', {phone_number: phone_number, session: session, childs_word: childs_word, childs_name: childs_name, indiv_score: indiv_score, level: level} , function(error, results, fields) {
         if (error) throw error;
-        return res.send({ error: false, data: results, message: 'New task has been created successfully.' });
+        return res.send({ error: false, data: results, message: 'Sessions table updated.' });
     });
 
+});
+
+app.post('/progress', function(req, res) {
+    let phone_number = req.body.phone_number;
+    let childs_word = req.body.childs_word;
+    let childs_name = req.body.childs_name;
+    let overall_score = req.body.overall_score;
+    let level = req.body.level;
+    let overall_sessions = req.body.overall_sessions;   
+    
+  //  var data = {phone_number: phone_number, childs_word: childs_word, childs_name: childs_name, overall_score: overall_score, level: level, overall_sessions: overall_sessions};
+    var sql = 'INSERT INTO progress (phone_number, childs_word, childs_name, overall_score, level, overall_sessions) VALUES ? ON DUPLICATE KEY UPDATE overall_score=VALUES(overall_score), level=VALUES(level), overall_sessions=VALUES(overall_sessions)';
+    var data = [[phone_number, childs_word, childs_name, overall_score, level, overall_sessions]];
+    con.query(sql, [data], function(error, results) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'Progress table updated.' });
+    })
 });
 
 // get information for feedback
@@ -52,12 +262,12 @@ app.get('/sessions/graph/:phone&:word', function(req, res) {
     let childs_word = req.params.word;
 
     if (!phone_number || !childs_word) {
-        return res.status(400).send({error:true, message: 'Please enter valid parameters.'})
+        return res.status(400).send({error:true, message: 'Please enter a phone number and word'})
     }
 
     con.query('SELECT session, indiv_score FROM sessions WHERE phone_number=? AND childs_word=?', [phone_number, childs_word], function(error, results, fields) {
         if (error) throw error;
-        return res.send({error: false, data: results, message: 'Information Taken'})
+        return res.send({error: false, data: results, message: 'Graph Information Taken'})
     });
 });
 
@@ -67,10 +277,10 @@ app.post('/accounts', function (req, res) {
     let phone_number = req.body.phone_number;
  
     if (!phone_number) {
-        return res.status(400).send({ error:true, message: 'Please provide task' });
+        return res.status(400).send({ error:true, message: 'Please enter a phone number' });
     }
  
-    con.query("INSERT INTO accounts SET ? ", { phone_number: phone_number }, function (error, results, fields) {
+    con.query("INSERT IGNORE INTO accounts SET ? ", { phone_number: phone_number }, function (error, results, fields) {
         if (error) throw error;
         return res.send({ error: false, data: results, message: 'New task has been created successfully.' });
     });
